b387a9890db9429bd2836735b3a87f3530724d1f
nonnynunny <135826781+nonnynunny@users.noreply.github.com>
2023-07-18 12:10:28 +0700

    Add files via upload

---
+++ src/Contact.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Layout } from "./App";
+
+function Contact() {
+    return (
+        <>
+        <Layout />
+            <h3>Ubon Ratchathani University</h3>
+            <div>
+                Ubon Ratchathani University
+                <br />
+                85 Sathonlamark Road
+                <br />
+                Warin Chamrap District
+                <br />
+                Ubon Ratchathani
+                <br />
+                Thailand 34190
+            </div>
+        </>
+    );
+}
+
+export default Contact;
